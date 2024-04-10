a2b84cf745ed78876e8fb0fe814f5fb5fe58ba0e
ObiamakaMaria <obiamaka0101@gmail.com>
2024-04-10 02:51:41 +0000

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Setting up a  python flask environent"""
+from flask import Flask, render_template, request
+from flask_babel import Babel  # type: ignore
+
+
+app = Flask(__name__)
+
+
+class Config:
+    '''
+    Setting up the configuration default values
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    Getting the locale language using the babel.localeselector deco
+    '''
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ The index page to be rendered """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
